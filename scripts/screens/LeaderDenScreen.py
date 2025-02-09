be521ddb1be42c15a4c58b4c2e277c6c688780eb
KiraDragoness <121829433+KiraDragoness@users.noreply.github.com>
2025-02-09 22:36:48 +0100

    Fix for outsider interactions crashing the game
    Fixed the name change for the outsider actions causing a crash.
    
    Fixed interactions with lost cats by .

--- scripts/screens/LeaderDenScreen.py
+++ scripts/screens/LeaderDenScreen.py
@@ -881,12 +881,12 @@ class LeaderDenScreen(Screens):
             },
         )
 
-        self.focus_button["hunt_down"] = UISurfaceImageButton(
+        self.focus_button["hunt"] = UISurfaceImageButton(
             ui_scale(pygame.Rect((0, 0), (121, 30))),
-            "screens.leader_den.hunt_down",
+            "screens.leader_den.hunt",
             get_button_dict(ButtonStyles.SQUOVAL, (121, 30)),
             object_id="@buttonstyles_squoval",
-            tool_tip_text="screens.leader_den.hunt_down_tooltip",
+            tool_tip_text="screens.leader_den.hunt_tooltip",
             tool_tip_text_kwargs={"r_c": self.focus_cat},
             container=self.focus_outsider_button_container,
             starting_height=3,
@@ -896,28 +896,28 @@ class LeaderDenScreen(Screens):
             },
         )
 
-        self.focus_button["drive_off"] = UISurfaceImageButton(
+        self.focus_button["drive"] = UISurfaceImageButton(
             ui_scale(pygame.Rect((0, 5), (121, 30))),
-            "screens.leader_den.drive_off",
+            "screens.leader_den.drive",
             get_button_dict(ButtonStyles.SQUOVAL, (121, 30)),
             object_id="@buttonstyles_squoval",
-            tool_tip_text="screens.leader_den.drive_off_tooltip",
+            tool_tip_text="screens.leader_den.drive_tooltip",
             tool_tip_text_kwargs={"r_c": self.focus_cat},
             container=self.focus_outsider_button_container,
             starting_height=3,
             manager=MANAGER,
             anchors={
                 "centerx": "centerx",
-                "top_target": self.focus_button["hunt_down"],
+                "top_target": self.focus_button["hunt"],
             },
         )
 
-        self.focus_button["invite_in"] = UISurfaceImageButton(
+        self.focus_button["invite"] = UISurfaceImageButton(
             ui_scale(pygame.Rect((0, 5), (121, 30))),
-            "screens.leader_den.invite_in",
+            "screens.leader_den.invite",
             get_button_dict(ButtonStyles.SQUOVAL, (121, 30)),
             object_id="@buttonstyles_squoval",
-            tool_tip_text="screens.leader_den.invite_in_tooltip",
+            tool_tip_text="screens.leader_den.invite_tooltip",
             tool_tip_text_kwargs={"r_c": self.focus_cat},
             container=self.focus_outsider_button_container,
             starting_height=3,
@@ -925,7 +925,7 @@ class LeaderDenScreen(Screens):
             visible=False,
             anchors={
                 "centerx": "centerx",
-                "top_target": self.focus_button["drive_off"],
+                "top_target": self.focus_button["drive"],
             },
         )
 
@@ -935,11 +935,11 @@ class LeaderDenScreen(Screens):
             and self.focus_cat.status
             not in ["kittypet", "loner", "rogue", "former Clancat"]
         ):
-            self.focus_button["invite_in"].set_text("screens.leader_den.search_for")
+            self.focus_button["invite"].set_text("screens.leader_den.search")
         else:
-            self.focus_button["invite_in"].set_text("screens.leader_den.invite_in")
+            self.focus_button["invite"].set_text("screens.leader_den.invite")
 
-        self.focus_button["invite_in"].show()
+        self.focus_button["invite"].show()
 
         self.focus_outsider_button_container.enable()
         if (
